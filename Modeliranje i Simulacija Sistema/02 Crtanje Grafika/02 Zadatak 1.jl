7d890b5cf396eff979fdf1b4c5e5d0a356beae68
Danijel Jovanović <danijel.xda@gmail.com>
2022-10-25 19:13:57 +0200

    MISS 02: 1

--- Modeliranje i Simulacija Sistema/02 Crtanje Grafika/02 Zadatak 1.jl
+++ Modeliranje i Simulacija Sistema/02 Crtanje Grafika/02 Zadatak 1.jl
@@ -1,12 +1,19 @@
 using Plots
 
-t  = 0:0.01:10;
-tp = rem.(t, 5)
+t = 0:0.01:10
+tp = rem.(tp, 5)
 
-y = (( 2 * tp) .* (tp .< 2)) .+ 
-    (  2 * ((tp .>= 2) .& (tp .< 4)) ) .+ 
-    ((-2 * tp .+ 10) .* ((tp .>= 4) .& (tp .<= 5)) );
+y1 = (2 * tp) .* (tp .< 2)
+y2 = 2 * ((tp .>= 2) .& (tp .< 4))
 
-plot(t, y, title="\nZadatak 1\n")
+# prava simetricna na y1 -> y = -2t sada je potrebno
+# izracunati na
+# y = k*t + n, uvrsti se jedna tacka npr A(4, 2)
+# n = 2 + 2 * 4 -> n = 10
+y3 = ((- 2 * tp) .+ 10) .* ((tp .>= 4) .& (tp .<= 5))
+
+y = y1 .+ y2 .+ y3
+
+plot(t, y, title="\nZadatak 1\n", xticks=0:10)
 xlabel!("tp")
 ylabel!("y")
