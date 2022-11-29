fe07cf8a84ebbeb1f16cc06d8f93afcfe51a8eab
Danijel Jovanović <danijel.xda@gmail.com>
2022-11-29 22:39:54 +0100

    MISS 02: Refine

--- Modeliranje i Simulacija Sistema/02 Crtanje Grafika/02 Zadatak 3.jl
+++ Modeliranje i Simulacija Sistema/02 Crtanje Grafika/02 Zadatak 3.jl
@@ -5,10 +5,9 @@ tp = rem.(t, 2)
 
 # jednacina prave kroz dve tacke za pravu
 y1 = 4/10 * t
-y = y1 .* (tp .<= 1)
+y = y1 .* (tp .<= 1) # ako je manje od 1 onda je y = 4, u suprotnom y = 0
 
-plot(t, y2, color=:red, label="y1", linestyle=:dash)
-plot!(t, y, title="\nZadatak 3\n", label="y", xticks=0:10, color=:blue)
+plot(t, y, title="\nZadatak 3\n", label="y", xticks=0:10, color=:blue)
 
 xlabel!("t")
 ylabel!("y")
