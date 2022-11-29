3613aa315e1d5de746294af2cd8c3b63e9d98dc6
Danijel Jovanović <danijel.xda@gmail.com>
2022-11-29 22:37:05 +0100

    MISS 02: Simplify

--- Modeliranje i Simulacija Sistema/02 Crtanje Grafika/02 Zadatak 3.jl
+++ Modeliranje i Simulacija Sistema/02 Crtanje Grafika/02 Zadatak 3.jl
@@ -5,10 +5,7 @@ tp = rem.(t, 2)
 
 # jednacina prave kroz dve tacke za pravu
 y1 = 4/10 * t
-
-y2 = 4 .* (tp .<= 1) # ako je manje od 1 onda je y = 4, u suprotnom y = 0
-
-y = min.(y1, y2)
+y = y1 .* (tp .<= 1)
 
 plot(t, y2, color=:red, label="y1", linestyle=:dash)
 plot!(t, y, title="\nZadatak 3\n", label="y", xticks=0:10, color=:blue)
