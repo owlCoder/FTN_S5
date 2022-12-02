c97c47af45ac9f4a06d71d81b12a8561f8ac7021
Danijel Jovanović <danijel.xda@gmail.com>
2022-12-02 11:51:31 +0100

    MISS 03: Same but different

--- Modeliranje i Simulacija Sistema/03 Numeričko Rešavanje ODJ/Zadatak 2.jl
+++ Modeliranje i Simulacija Sistema/03 Numeričko Rešavanje ODJ/Zadatak 2.jl
@@ -15,18 +15,13 @@ function diferencijalnaJednacina!(dx, x, p, t)
     A, B, C = p
     F = signal(t)
 
-     dx[1] = x[3]
-     dx[2] = x[4]
-     dx[3] = -3 * x[3] - C * (x[3] - x[4]) - B * (x[1] - x[2])
-     dx[4] = (C * (x[3] - x[4]) - A * x[2] + B * (x[1] - x[2]) + F) / 2
+    dx[1] = x[2]
+    dx[2] = -3 * x[2] - C * (x[2] - x[4]) - B * (x[1] - x[3])
+    dx[3] = x[4]
+    dx[4] = 1 / 2 * (C * (x[2] - x[4]) - A * x[3] + B * (x[1] - x[3]) + F)
 end
 
 # main.jl
-
-# provera signala
-# t = 0:0.01:9
-# plot(t, signal(t), xticks =0:9)
-
 tspan = (0.0, 9.0)
 x0 = [0.0, 0.0, 0.0, 0.0]
 parametri = (12.0, 8.0, 4.0)
